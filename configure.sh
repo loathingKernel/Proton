c7a9912df40d5e74d5d49ab097fb5cb63197524b
Stelios Tsampas <loathingkernel@gmail.com>
2025-08-15 22:15:38 +0300

    dxvk-sarek: build: add dxvk-sarek make target

--- configure.sh
+++ configure.sh
@@ -186,6 +186,9 @@ function configure() {
     if [[ -n "$arg_without_tts" ]]; then
       echo "WITHOUT_TTS := 1"
     fi
+    if [[ -n "$arg_without_sarek" ]]; then
+      echo "WITHOUT_SAREK := 1"
+    fi
 
     echo "HOST_CFLAGS := ${CFLAGS:--O2 -march=nocona -mtune=core-avx2}"
     echo "HOST_RUSTFLAGS := ${RUSTFLAGS:--Copt-level=3 -Ctarget-cpu=nocona}"
@@ -212,6 +215,7 @@ arg_relabel_volumes=""
 arg_enable_ccache=""
 arg_enable_bear=""
 arg_without_tts=""
+arg_without_sarek=""
 arg_help=""
 invalid_args=""
 function parse_args() {
@@ -262,6 +266,8 @@ function parse_args() {
       arg_enable_bear="1"
     elif [[ $arg = --without-tts ]]; then
       arg_without_tts="1"
+    elif [[ $arg = --without-sarek ]]; then
+      arg_without_sarek="1"
     elif [[ $arg = --proton-sdk-image ]]; then
       val_used=1
       arg_protonsdk_image="$val"
@@ -320,6 +326,8 @@ usage() {
   "$1" "    --enable-bear Invokes make via bear creating compile_commands.json."
   "$1" ""
   "$1" "    --without-tts Disables text-to-speech libraries (OpenFST, VOSK, Kaldi and Piper)"
+  "$1" ""
+  "$1" "    --without-sarek Disables dxvk-sarek"
   "$1" ""
   "$1" "  Steam Runtime"
   "$1" "    Proton builds that are to be installed & run under the steam client must be built with"
